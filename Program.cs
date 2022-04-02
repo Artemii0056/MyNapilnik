552148f5bff1c2fe72bd839c663dad39ae73805d
Artemii0056 <aza_art@mail.ru>
2022-04-02 19:04:24 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode7
+{
+    class Program
+    {
+        public static void CreateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void InstallChance()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int DefineSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+}
